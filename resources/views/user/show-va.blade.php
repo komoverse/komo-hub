63079dde32f672e14572f222c2782217a77e4112
nikkoagustino <nikkoagustino@gmail.com>
2022-07-21 14:16:02 +0700

    show VA

--- resources/views/user/show-va.blade.php
+++ resources/views/user/show-va.blade.php
@@ -1,6 +1,6 @@
 @extends('user.template')
 @section('content')
-<?php var_dump($pg_data); ?>
+{{-- <?php var_dump($pg_data); ?> --}}
 <div class="row">
     <div class="col-12">
         <h1>Payment via Indonesian Rupiah (Virtual Account)</h1>
@@ -18,7 +18,7 @@
             </tr>
             <tr>
                 <td>No VA</td>
-                {{-- <td><span id="noVA">{{ $pg_data->address }}</span> --}}
+                <td><span id="noVA">{{ $pg_data->address }}</span>
                     <button class="btn btn-sm ms-3 btn-info" onclick="copyToClipboard('noVA')"><i class="fas fa-copy"></i></button>
                 </td>
             </tr>
