910f428ebe4d63be0c2758ffbf087bf07c150fba
nikkoagustino <nikkoagustino@gmail.com>
2022-08-03 08:14:55 +0000

    fix VA on production

--- config/api_key.php
+++ config/api_key.php
@@ -6,7 +6,7 @@ return [
   'pg_api_key' => env('PG_API_KEY', false),
   'pg_account_id' => env('PG_ACCOUNT_ID', false),
   'pg_endpoint' => env('PG_ENDPOINT', false),
-  'pg_bank_id' => env('PG_BANK_ID', 'demo'),
+  'pg_bank_id' => env('PG_VA_BANK_ID', 'demo'),
   'g_recaptcha_site_key' => env('G_RECAPTCHA_SITE_KEY', false),
   'g_recaptcha_secret_key' => env('G_RECAPTCHA_SECRET_KEY', false),
 ];
