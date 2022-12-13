a59584fcd0f44f521f555427e0d07840ceffbabd
Nikko Agustino <nikkoagustino@gmail.com>
2022-12-13 13:28:22 +0700

    change verchiel card and add environment notice

--- resources/views/user/before-login-template.blade.php
+++ resources/views/user/before-login-template.blade.php
@@ -8,6 +8,7 @@
         <script src="https://kit.fontawesome.com/b71ce7388c.js" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+        <script src="https://files.komoverse.io/jquery.komo-env.js"></script>
         <script src="https://unpkg.com/@solana/web3.js@latest/lib/index.iife.min.js"></script>
       <script src="https://cdn.jsdelivr.net/gh/ethereumjs/browser-builds/dist/ethereumjs-tx/ethereumjs-tx-1.3.3.min.js"></script>
         <style>
--- resources/views/user/template.blade.php
+++ resources/views/user/template.blade.php
@@ -13,6 +13,7 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
         <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
+        <script src="https://files.komoverse.io/jquery.komo-env.js"></script>
         <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css">
         <style>
             body {
